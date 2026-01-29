25cbe6fdc8801830058043163c6eda541d822934
Erica Ambrosio <152650668+ericala9@users.noreply.github.com>
2026-01-29 18:15:08 -0300

    Update 00_requirements.R

--- src/scripts/00_requirements.R
+++ src/scripts/00_requirements.R
@@ -2,25 +2,58 @@
 #  1º HACKATHON EM CONTROLE SOCIAL: DESAFIO PARTICIPA DF - ACESSO À INFORMAÇÃO 
 # ==============================================================================
 # Script: src/scripts/00_requirements.R
-# Objetivo: Restaurar o ambiente do projeto, carregando os pacotes necessários
-# conforme o arquivo renv.lock
+# Objetivo: Restaurar o ambiente do projeto de forma robusta.
+# Estratégia: Tenta usar renv::restore(). Se falhar (erro de rede/sistema),
+# muda automaticamente para instalação direta 
 # Data: 2026-01
 # ==============================================================================
 
 # ------------------------- Instalação de dependências -------------------------
 
 if (!requireNamespace("renv", quietly = TRUE)) {
-  install.packages("renv")
+  install.packages("renv") 
 }
 
-renv::restore(prompt = FALSE)
+## Plano A - Instalação por meio do renv ---------------------------------------
+sucesso_renv <- tryCatch({
+  renv::restore(prompt = FALSE)
+  TRUE 
+}, error = function(e) {
+  return(FALSE)
+})
+
+## Plano B - Instalação "manual" -----------------------------------------------
+
+# Lista de pacotes utilizados na solução
+pacotes_essenciais <- c(
+  "dplyr",
+  "jsonlite",
+  "openxlsx",
+  "readr",
+  "rstudioapi",
+  "stringi",
+  "stringr",
+  "tidyr",
+  "tidytext",
+  "tm",
+  "tools"
+)
+
+if (!sucesso_renv) {
+  for (pkg in pacotes_essenciais) {
+    if (!requireNamespace(pkg, quietly = TRUE)) {
+      tryCatch({
+        install.packages(pkg) 
+      }, error = function(e) {
+        message(paste("Erro ao instalar", pkg, "-", e$message))
+      })
+    }
+  }
+}
 
 # ------------------------------------------------------------------------------
 #                                 Próximo passo 
 # ------------------------------------------------------------------------------
-# A) Para avaliação da solução:
+# A) Para avaliação da solução (Execução Principal):
 #     src/scripts/05_classificar_textos.R
-#
-# B) Para reconstrução completa do projeto:
-#     src/scripts/01_download_nomes_ibge.R
 # ------------------------------------------------------------------------------
